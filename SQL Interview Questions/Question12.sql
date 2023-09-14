111a9b221949835d25dfb47d64f7920ff87d7684
sekar N <sekarnagarajan1234@gmail.com>
2023-09-14 11:25:29 +0530

    sql

---
+++ SQL Interview Questions/Question12.sql
@@ -0,0 +1,4 @@
+-- Write an SQL query to print all Worker details from the Worker table order by FIRST_NAME Ascending and DEPARTMENT Descending.
+
+
+SELECT * FROM WorkerDetails ORDER BY FIRST_NAME ASC , DEPARTMENT DESC
