d4a79c63e3cd333e65beed7bb73c2ce9cb73f40a
sekar N <sekarnagarajan1234@gmail.com>
2023-09-14 11:48:32 +0530

    sql

--- SQL Interview Questions/Question21.sql
+++ SQL Interview Questions/Question21.sql
@@ -1,3 +1,3 @@
 -- Write an SQL query to fetch the count of employees working in the department ‘Admin’.
 
-SELECT COUNT(*) COUNT FROM WorkerDetails WHERE DEPARTMENT = 'Admin'
+SELECT COUNT(*) FROM WorkerDetails WHERE DEPARTMENT = 'Admin'
