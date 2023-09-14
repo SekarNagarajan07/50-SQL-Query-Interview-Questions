b55102b10720d95ebc34eb5538171e11c9a693bd
sekar N <sekarnagarajan1234@gmail.com>
2023-09-14 11:27:50 +0530

    sql

---
+++ SQL Interview Questions/Question13.sql
@@ -0,0 +1,3 @@
+-- Write an SQL query to print details for Workers with the first names “Vipul” and “Satish” from the Worker table.
+
+SELECT * FROM WorkerDetails WHERE FIRST_NAME IN ('Vipul','Satish')
